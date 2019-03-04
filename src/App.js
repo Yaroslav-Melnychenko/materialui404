38c29be2806035d8945b9115a7f1d7c94a490947
Yaroslav Melmychenko <yaroslav.melnychenko@chisw.com>
2019-03-04 14:26:27 +0200

    update code after eslint rules

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 import Main from './components/main';
 
-class App extends Component {
-  render() {
-    return (
-      <Main />
-    );
-  }
-}
-
+const App = () => <Main />;
 export default App;
--- src/components/instruments/instruments.js
+++ src/components/instruments/instruments.js
@@ -1,23 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Selected from './selected';
 import './instruments.scss';
 
-class Instruments extends Component {
-
-    render(){
-      return(
-        <div className="color-panel-container">
-            <div className="panel-heading">
-                Selected colors
-            </div>
-            <div className="color-panel">
-              {
-                this.props.colors.map((color, i) => <Selected key={i} color={color} />)
-              }
-            </div>
-        </div>
-      );
-    }
-  
-  }
-  export default Instruments;
+const Instruments = (props) => {
+  const { colors } = props;
+  return(
+    <div className="color-panel-container">
+      <div className="panel-heading">Selected colors</div>
+      <div className="color-panel">
+        {
+          colors.map(color => <Selected key={color.id} color={color} />)
+        }
+      </div>
+    </div>
+  );
+};
+export default Instruments;
